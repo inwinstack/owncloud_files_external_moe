33d5f9c9f43d4085d268763b18253753e71e67d4
JamieHo3312 <jamie.h@inwinstack.com>
2017-08-07 11:53:28 +0800

    [Feature] Add instructions template in external sotage settings.

--- js/help.js
+++ js/help.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
     $(".files_external_info").click(function(){
-        OC.dialogs.info(t('files_external_moe', 'Please read operation document.'), t('files_external_moe', 'Instructions'));
+        //OC.dialogs.info(t('files_external_moe', 'Please read operation document.'), t('files_external_moe', 'Instructions'));
+        $.colorbox({
+                opacity:0.4,
+                transition:"elastic",
+                speed:100,
+                width:"70%",
+                height:"70%",
+                href: OC.filePath('files_external_moe', '', 'help.php'),
+                //href: OC.filePath('firstrunwizard', '', 'wizard.php'),
+                //onComplete : function(){
+                //      if (!SVGSupport()) {
+                //              replaceSVG();
+                //      }
+                //},
+                //onClosed : function(){
+                //      $.ajax({
+                //      url: OC.filePath('firstrunwizard', 'ajax', 'disable.php'),
+                //      data: ""
+                //      });
+                //}
+        });
     });
 });
